c936db966691546328dad80b07f8bc120b8c9d63
tyrant0407 <amankushwaha1331@gmail.com>
2024-04-10 11:11:00 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport')
+const localStrategy = require('passport-local')
+const userModel = require('./users.js')
+passport.use(new localStrategy(userModel.authenticate()));
+
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', isLoggedIn,function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
@@ -10,16 +15,52 @@ router.get('/', function (req, res, next) {
 router.get('/register', function (req, res) {
   res.render('register', { title: 'Register' });
 })
-
+router.post('/register', function(req, res, next) {
+  const user = new userModel({
+    username:req.body.username,
+    email:req.body.email,
+    accountType:req.body.isSeller == 'on' ? 'seller':'buyer'
+  })
+  userModel.register(user,req.body.password)
+  .then(function(registeredUser){
+  passport.authenticate('local')(req,res, function(err){
+    if(registeredUser.accountType=='seller'){
+      res.redirect('/createProduct')
+    }
+    else{
+      res.redirect('/')
+    }
+     })
+  })
+});
 router.get('/login', function (req, res) {
   res.render('login', { title: 'Login' });
 })
+router.post('/login', passport.authenticate("local",{
+  successRedirect:"/",
+  failureRedirect: "/login",
+}) ,function(req, res) {
+});
 
-
-router.get('/createProduct', function (req, res) {
+router.get('/createProduct',isLoggedIn,isSeller,function (req, res) {
   res.render('createProduct', { title: 'Create Product' });
 })
 
+router.get('/logout',function(req, res,next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/login');
+  });
+});
 
 
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()) return next();
+  res.redirect("/login")
+}
+
+function isSeller(req,res,next){
+  if(req.user.accountType == 'seller') return next();
+  else res.redirect('/')
+}
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const plm = require('passport-local-mongoose')
 
-// TODO: connect to mongoose
+mongoose.connect("mongodb://127.0.0.1:27017/N17Ecommerce")
+
 
 const userSchema = mongoose.Schema({
   username: String,
@@ -17,6 +19,6 @@ const userSchema = mongoose.Schema({
   } ],
   
 })
-
+userSchema.plugin(plm)
 module.exports = mongoose.model('user', userSchema)
 
