ad56760d743a54afa82d01d055ab6c1ffa7f8f45
tyrant0407 <amankushwaha1331@gmail.com>
2024-05-16 10:51:41 +0530

    error fixed

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,9 @@ const upload = require('./multer')
  
 /* GET home page. */
 router.get('/', isloggedIn, async function (req, res, next) {
+
   const allProducts = await productModel.find()
+
   res.render('index', { title: 'Express', allProducts });
 });
 
@@ -63,9 +65,7 @@ router.get('/login', function (req, res) {
 // })
 
 
-router.post(
-  '/login',
-  passport.authenticate('local', {
+router.post('/login',passport.authenticate('local',{
     failureRedirect: '/login',
   }),
   (req, res, next) => {
@@ -111,7 +111,6 @@ router.post('/createProduct', isloggedIn, isSeller, upload.array('image'), async
 
 })
 
-<<<<<<< HEAD
 router.get('/cart',isloggedIn, async (req, res, next) => {
   const userCart = await cartModel.findOne({
     user:req.user._id
@@ -163,25 +162,7 @@ else{
 await userCart.save()
 
 res.redirect('back')
-=======
-router.get('/cart', isloggedIn, async (req, res, next) => {
-  const userCart = await cartModel.findOne({
-    user: req.user._id
-  }).populate('products').populate({
-    path: "products",
-    populate: 'product'
-  })
 
-  let totalPrice = 0
-
-  userCart.products.forEach(cartProduct => {
-    totalPrice += cartProduct.product.price * (cartProduct.quantity == 0 ? 1 : cartProduct.quantity)
-  })
-
-
->>>>>>> 082648b03969898fa3a2baa1828de1674c4fe79a
-
-  res.render('cart', { userCart, totalPrice })
 })
 
 router.get('/profile', (req, res, next) => {
@@ -196,7 +177,6 @@ router.post('/updatedQuantity',isloggedIn,async (req, res, next) => {
   if(!product) return res.status(404).send('product not found')
   userCart.price = userCart.price - (product.product.price*product.quantity) + (product.product.price * req.body.quantity )
 
-<<<<<<< HEAD
   await cartProductModel.findOneAndUpdate({_id:req.body.cartProductId},
   {quantity:req.body.quantity}),
   res.json({message:"quantity updated",price:userCart.price})
@@ -208,59 +188,8 @@ router.post('/updatedQuantity',isloggedIn,async (req, res, next) => {
     if(!userCart) return res.status(404).send('cart not found')
     let product = await cartProductModel.findById(req.params.cartProductId).populate('product')
     if(!product) return res.status(404).send('product not found')
-    userCart.products = user.products.filter(p=> p.toString() !== req.params.cartProductId);
+    userCart.products = userCart.products.filter(p=> p.toString() !== req.params.cartProductId);
     await userCart.save();
-=======
-router.get('/addToCart/:productId', isloggedIn, async (req, res, next) => {
-  const productId = req.params.productId
-
-  let userCart = await cartModel.findOne({
-    user: req.user._id
-  })
-
-  if (!userCart) {
-    userCart = await cartModel.create({
-      user: req.user._id,
-    })
-  }
-
-
-  let newCartProduct = await cartProductModel.findOne({
-    product: productId,
-    _id: { $in: userCart.products }
-  })
-
-  if (newCartProduct) {
-    newCartProduct.quantity = newCartProduct.quantity + 1
-    await newCartProduct.save()
-  }
-  else {
-    newCartProduct = await cartProductModel.create({
-      product: productId,
-      quantity: 1
-    })
-    userCart.products.push(newCartProduct._id)
-    await userCart.save()
-  }
-
-
-  res.redirect('back')
-
-})
-
-router.post('/updateQuantity', isloggedIn, async (req, res, next) => {
-  await cartProductModel.findOneAndUpdate({ _id: req.body.cartProductId }, {
-    quantity: req.body.quantity
-  })
-  res.json({ message: "quantity updated" })
-})
-
-router.get('/remove/:cartProductId', isloggedIn, async (req, res, next) => {
-  await cartProductModel.findOneAndDelete({ _id: req.params.cartProductId })
-  res.redirect('back')
-})
-
->>>>>>> 082648b03969898fa3a2baa1828de1674c4fe79a
 
     await cartProductModel.findOneAndDelete({_id:req.params.cartProductId}),
     res.redirect('back')
